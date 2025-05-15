d5a499749aa33eb770ba693ca7da63a9abe01233
Gabriel Taborda Farias <gabrielfariasbass@gmail.com>
2025-05-15 00:25:59 -0300

    type safing navigator

--- src/utils/helper/browser.ts
+++ src/utils/helper/browser.ts
@@ -21,14 +21,16 @@ export const scrollToID = (id: string) => {
 };
 
 export const getPlatform = () => {
-  const userAgent = navigator.userAgent || String(navigator.vendor);
+  if (typeof navigator !== "undefined") {
+    const userAgent = navigator.userAgent || String(navigator.vendor);
 
-  if (/windows phone/i.test(userAgent)) return "Windows Phone";
-  if (/android/i.test(userAgent)) return "Android";
-  if (/iPad|iPhone|iPod/.test(userAgent)) return "iOS";
-  if (/Macintosh/.test(userAgent)) return "macOS";
-  if (/Windows/.test(userAgent)) return "Windows";
-  if (/Linux/.test(userAgent)) return "Linux";
+    if (/windows phone/i.test(userAgent)) return "Windows Phone";
+    if (/android/i.test(userAgent)) return "Android";
+    if (/iPad|iPhone|iPod/.test(userAgent)) return "iOS";
+    if (/Macintosh/.test(userAgent)) return "macOS";
+    if (/Windows/.test(userAgent)) return "Windows";
+    if (/Linux/.test(userAgent)) return "Linux";
+  }
 
   return "Unknown";
 };
